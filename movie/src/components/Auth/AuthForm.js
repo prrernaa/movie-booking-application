79df8590b41da05af8ac489d8258fd3d67d188da
Prerna <prernajangra1124@gmail.com>
2023-05-11 19:37:41 +0530

    moviedata app insex change

--- movie/src/components/Auth/AuthForm.js
+++ movie/src/components/Auth/AuthForm.js
@@ -1,62 +1,73 @@
-import { Button, Dialog, FormLabel, IconButton, TextField, Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { Box, Button, Dialog, FormLabel, IconButton, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-const labelStyle={mt:1,mb:1};
-const AuthForm = ({onSubmit ,isAdmin }) => {
-    const [inputs, setinputs] = useState({
-        name:"",
-        email:"",
-        password:"",
-    });
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
+import { useNavigate } from 'react-router-dom';
 
+const labelStyle = {mt:1, mb:2}
+
+const AuthForm = ({onSubmit, isAdmin}) => {
+
+    const navigate = useNavigate();
+    const crossHandler = () => {
+        navigate("/")
+    }
+
+    const [inputs, setinputs] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
     const [isSignup, setisSignup] = useState(false);
-    
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
         setinputs((prevState)=>({
             ...prevState,
-            [e.target.name] : e.target.value,
-
+            [e.target.name]: e.target.value,
+            // [e.target.email]: e.target.email,
+            // [e.target.password]: e.target.password
         }))
     }
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        onSubmit({inputs,signup:isAdmin?false:isSignup});
-
-        
+        onSubmit({inputs, signup: isAdmin?false:isSignup});
     }
-
-  return (
-   <Dialog paperProps={{style:{borderRadius:20}}}open={true}>
-   <Box sx={{ml:'auto',padding:1}}> 
-    <IconButton>
-        <CloseRoundedIcon/>
-    </IconButton>
     
-   </Box>
-    <Typography variant='h4' textAlign={'center'}>
-    {isSignup?'Signup':'login'}
-    </Typography>
-    <form onSubmit={handleSubmit}>
-        <Box display="flex" justifyContent={'center'} flexDirection='column' width={400} margin='auto' alignItems={'center'} padding={4}>
-           { !isAdmin &&isSignup &&<><FormLabel sx={labelStyle}>Name</FormLabel>
-            <TextField  value={inputs.name} onChange={handleChange}  variant='standard' margin='normal'  type={'text'} name='name'/> </>
-            
-            }
-
-
-            <FormLabel sx={labelStyle}>Email</FormLabel>
-            <TextField value={inputs.email} onChange={handleChange} variant='standard' margin='normal'  type={'email'} name='email'/>
-            <FormLabel sx={labelStyle}>Password</FormLabel>
-            <TextField  value={inputs.password} onChange={handleChange}  variant='standard' margin='normal'  type={'password'} name='password'/>
-            <Button sx={{mt:2,borderRadius:10,bgcolor:'#2b2d42'}}type="submit" fullWidth variant='contained'> {isSignup?'Signup':'login'} </Button>
-     {  !isAdmin &&    ( <Button onClick={()=>{
-                setisSignup(!isSignup)
-            }} sx={{mt:2,borderRadius:10}} fullWidth > Switch To {isSignup?"Login":"Signup"}</Button>)}
-            
+  return (
+    <Dialog PaperProps={{style:{borderRadius:20}}} open={true}>
+        <Box sx={{ ml:'auto', padding:1 }}>
+            <IconButton onClick={crossHandler}>
+            <ClearRoundedIcon />
+            </IconButton>
         </Box>
-    </form>
-   </Dialog>
+        <Typography variant='h4' textAlign={'center'} >
+         {isSignup?"Signup":"Login"}
+        </Typography>
+        <form onSubmit={handleSubmit}>
+            <Box padding={6} display={'flex'} justifyContent={'center'} flexDirection={'column'} width={400} margin={'auto'} alignContent={'center'}>
+
+                { !isAdmin && isSignup && (
+                    <>
+                    <FormLabel sx={labelStyle}>Name</FormLabel>
+                <TextField value={inputs.name} onChange={handleChange} variant='standard' margin='normal' type={'name'} name='name' />
+                </>
+                )}
+                
+                <FormLabel sx={labelStyle}>Email</FormLabel>
+                <TextField value={inputs.email} onChange={handleChange} variant='standard' margin='normal' type={'email'} name='email' />
+
+                <FormLabel sx={labelStyle}>Password</FormLabel>
+                <TextField value={inputs.password} onChange={handleChange} variant='standard' margin='normal' type={'password'} name='password' />
+
+                <Button sx={{ mt:2, borderRadius:10, bgcolor:"#2b2d42"}} type='submit' fullWidth variant='contained'>
+                    {isSignup?"Signup":"Login"}
+                </Button>
+
+                { !isAdmin && 
+                    (<Button onClick={() => setisSignup(!isSignup)} sx={{ mt:2, borderRadius:10}} fullWidth >
+                    Switch to {isSignup?"Login":"Signup"}
+                </Button>)}
+            </Box>
+        </form>
+    </Dialog>
   )
 }
 
--- movie/src/index.js
+++ movie/src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios';
-import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { store } from './store';
+// import { LocalizationProvider } from '@mui/x-date-pickers';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL="http://localhost:5000";
+axios.defaults.baseURL="https://movie-booking-app-0wnl.onrender.com"
 root.render(
   <React.StrictMode>
   <BrowserRouter>
   <Provider store={store}>
+  
     <App />
-    </Provider>
-    </BrowserRouter>
+    
+  </Provider>
+  </BrowserRouter>
   </React.StrictMode>
+  
 );
